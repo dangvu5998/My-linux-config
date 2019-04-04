aeccbf5e80fbfa97c6c43ef6e982cf998e861020
dangvu5998 <19854233+dangvu5998@users.noreply.github.com>
2019-04-05 00:26:05 +0700

    Create common-package.sh

---
+++ common-package.sh
@@ -0,0 +1,13 @@
+sudo apt install fonts-firacode
+sudo apt install neofetch
+
+pip install powerline-shell
+
+# Need to add config to .bashrc file and config terminal font
+#function _update_ps1() {
+#    PS1=$(powerline-shell $?)
+#}
+#
+#if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then
+#    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+#fi
